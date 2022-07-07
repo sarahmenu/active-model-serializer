a537bc65dce70c5bcb04ca7aa71dcfe2b4949427
Sarah Menu <sarah.menu@gmail.com>
2022-07-07 12:10:58 +0200

    Routing and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  concern :api_base do
+    resources :employees
+    resources :managers
+  end
+
+  namespace :v1 do
+    concerns :api_base
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Employee.destroy_all
+Manager.destroy_all
+
+puts "creating managers"
+managers = (1..20).map do
+  Manager.create!(
+    name: "manager"
+  )
+end
+
+puts "creating empoyees"
+employee = (1..50).map do
+  Employee.create!(
+    name: "employee",
+    email: "emp@gmail.com",
+    dob: "17-01-1988",
+    mobile: "8879544321",
+    designation: "Senior Developer",
+    salary: 35_000,
+    manager: managers.sample
+  )
+end
